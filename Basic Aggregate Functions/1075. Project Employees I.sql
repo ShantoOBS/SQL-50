d3dce41ab257bcf8d06f319d1f5cea0f6d0d2930
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-08-09 21:44:38 +0600

    Update 1075. Project Employees I.sql

--- Basic Aggregate Functions/1075. Project Employees I.sql
+++ Basic Aggregate Functions/1075. Project Employees I.sql
@@ -1,3 +1,6 @@
+/*      Company : 
+        Leetcode :https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50   */
+
 # Write your MySQL query statement below
 
 select p.project_id, ROUND(SUM(experience_years)/count(experience_years),2) as average_years
