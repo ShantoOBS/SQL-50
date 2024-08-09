135c82b44de61b2376d9cd20a6363c67f1e7612c
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-08-09 21:41:22 +0600

    Update 1978. Employees Whose Manager Left the Company.sql

--- Subqueries/1978. Employees Whose Manager Left the Company.sql
+++ Subqueries/1978. Employees Whose Manager Left the Company.sql
@@ -1,8 +1,7 @@
 
 /*      Company : Google,Amazon,Microsoft,Meta,Bloomberg 
-        Leetcode :https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50   */
-
-
+        Leetcode :https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50   
+*/
 
 # Write your MySQL query statement below
 
